a23d10f982e17d307ab63f49d38016e564120b67
SR-G <serge.simon@gmail.com>
2018-03-25 23:57:59 +0200

    Upgrade

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 SOURCEDIR=.
 SOURCES := $(shell find $(SOURCEDIR) -name '*.go')
 
-BINARY=java-folders-cleaner
+BINARY=cleaner
 PWD=$(shell pwd)
 
 VERSION=1.0.0
@@ -22,10 +22,31 @@ build:
 	time go install ${PACKAGE}
 
 install:
-	time go install ${LDFLAGS} ${PACKAGE}
+	GOARCH=amd64 GOOS=linux go install ${LDFLAGS} ${PACKAGE}
+	GOARCH=amd64 GOOS=windows go install ${PACKAGE}
 
 clean:
-	[ -f bin/${BINARY} ] && rm -f bin/${BINARY}
+	-@rm -f bin/${BINARY} 2>/dev/null || true
+	-@rm -rf bin/linux 2>/dev/null || true
+	-@rm -rf bin/windows_amd64 2>/dev/null || true
+
+distribution: clean install
+	mkdir /go/bin/linux/ 
+	mv /go/bin/${BINARY} /go/bin/linux/
+	# cp /go/src/main/resources/sol.json /go/bin/linux/ 
+	# cp /go/src/main/resources/sol.json /go/bin/windows_amd64/
+	# cp /go/src/script/*.bat /go/bin/windows_amd64
+	cd /go/bin/ ; zip -r -9 ${BINARY}.zip ./linux ; zip -r -9 ${BINARY}.zip ./windows_amd64
+
+test:
+	-@mkdir bin/target 2>/dev/null || true
+	-@mkdir bin/bin 2>/dev/null || true
+	-@touch bin/bin/test.class 2>/dev/null || true
+	-@touch bin/target/test.jar 2>/dev/null || true
+	-@touch bin/target/test.log 2>/dev/null || true
+	-@touch bin/test.log 2>/dev/null || true
+	-@touch bin/toto.class 2>/dev/null || true
+	-@mkdir bin/logs 2>/dev/null || true
 
 run:
 	bin/cleaner
@@ -35,8 +56,8 @@ init:
 	glide update
 	glide install
 	
-test:
-	go test -v tensin.org/cleaner/core/utils
+# test:
+# 	go test -v tensin.org/cleaner/core/utils
 
 docker:
 	docker run --rm -it -v ${PWD}:/go tensin-app-golang /bin/bash
--- src/tensin.org/cleaner/commands/root.go
+++ src/tensin.org/cleaner/commands/root.go
@@ -2,21 +2,94 @@ package commands
 
 import (
 	"fmt"
+	"os"
+    "path/filepath"
+	"strings"	
+	
 	"github.com/spf13/cobra"
 )
 
-// ConfigurationFileName is the configuration file name that will be used
-var ConfigurationFileName string
+/*
+func RemoveContents(dir string) error {
+    d, err := os.Open(dir)
+    if err != nil {
+        return err
+    }
+    defer d.Close()
+    names, err := d.Readdirnames(-1)
+    if err != nil {
+        return err
+    }
+    for _, name := range names {
+        err = os.RemoveAll(filepath.Join(dir, name))
+        if err != nil {
+            return err
+        }
+    }
+    return nil
+}
+*/
 
 // RootCmd is the main command = the program itself
 var RootCmd = &cobra.Command{
 	Use:   "java-folders-cleaner",
 	Short: "java-folders-cleaner",
 	Long:  `java-folders-cleaner`,
+	Run: func(cmd *cobra.Command, args []string) {
+		searchDir := "."
+		patterns := make([]string, 0)
+		/*
+		*/
+		patterns = append(patterns, "bin")
+		patterns = append(patterns, "target")
+		patterns = append(patterns, "*.log")
+		patterns = append(patterns, "*.class")
+
+		fmt.Println("Now cleaning java useless items from [" + searchDir + "] with patterns [" + strings.Join(patterns, ", ") + "]")
+
+		fileList := make([]string, 0)
+		e := filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
+			fileList = append(fileList, path)
+			return err
+		})
+
+		if e != nil {
+			panic(e)
+		}
+
+		for _, path := range fileList {
+			// fmt.Println("path [" + path + "]")
+			for _, pattern := range patterns {
+				matched, err := filepath.Match(pattern, path)
+				if err != nil {
+					fmt.Println(err)
+				} 
+				if matched {
+					// fmt.Println("- pattern [" + pattern + "] / [" + path + "]")
+				} else {
+					// fmt.Println(". pattern [" + pattern + "] / [" + path + "]")
+				}
+				if matched {
+					fullPath, _ := filepath.Abs(path)
+					fi, _ := os.Stat(fullPath)
+					switch mode := fi.Mode(); {
+					case mode.IsDir():
+						fmt.Println("Remove directory [" + path + "]")
+						os.RemoveAll(fullPath)
+					case mode.IsRegular():
+						fmt.Println("Remove file [" + path + "]")
+						os.Remove(fullPath)
+					}					
+				}
+			}
+		}
+
+		// os.RemoveAll()
+	  },
 }
 
 func init() {
 	// RootCmd.PersistentFlags().StringVarP(&ConfigurationFileName, "configuration", "", "watchthatpage.json", "Configuration file name. Default is binary name + .json (e.g. 'watchthatpage.json'), in the same folder than the binary itself")
 	// daemon.Server.Start()
-	fmt.Println("start")
+	// fmt.Println("start")
 }
